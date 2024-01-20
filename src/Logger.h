5a322a845c316eeed25ad4ae4f753331f7ac1416
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2024-01-20 14:42:17 -0700

    Update copyright year

--- src/Logger.h
+++ src/Logger.h
@@ -3,7 +3,7 @@
 // This file is part of sc4-auto-save, a DLL Plugin for SimCity 4
 // that automatically saves a city at user-specified intervals.
 //
-// Copyright (c) 2023 Nicholas Hayes
+// Copyright (c) 2023, 2024 Nicholas Hayes
 //
 // This file is licensed under terms of the MIT License.
 // See LICENSE.txt for more information.
